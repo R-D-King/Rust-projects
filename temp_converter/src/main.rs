97ab7aee81c9b8af1c3bfaa3ddf717169bb375e8
Hassan <hahsfm@gmail.com>
2024-09-30 20:36:27 +0300

    new project
    Convert temperatures between Fahrenheit and Celsius.

---
+++ temp_converter/src/main.rs
@@ -0,0 +1,60 @@
+use std::io;
+
+fn main() {
+    println!("This is an app to Convert temperatures between Fahrenheit and Celsius.");
+    'input_check: loop {
+        println!("First select your temeprature scale: \n [1] from Fahrenheit to Celsius \n [2] from Celsius to Fahrenheit");
+
+        let mut temp_type = String::new();
+
+        io::stdin()
+            .read_line(&mut temp_type)
+            .expect("Failed to read line");
+
+        let temp_type: u8 = match temp_type.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if temp_type == 1 {
+            println!("You Chose to convert from Fahrenheit to Celsius.\n");
+            println!("Now enter your temprature in Fahrenheit:");
+            let mut deg_num = String::new();
+            io::stdin()
+                .read_line(&mut deg_num)
+                .expect("Failed to read line");
+
+            let deg_num: f64 = match deg_num.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            println!("{}", temp_converter(1, deg_num));
+        } else if temp_type == 2 {
+            println!("You Chose to convert from Celsius to Fahrenheit.\n");
+            println!("Now enter your temprature in Celsius:");
+            let mut deg_num = String::new();
+            io::stdin()
+                .read_line(&mut deg_num)
+                .expect("Failed to read line");
+
+            let deg_num: f64 = match deg_num.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            println!("{}", temp_converter(2, deg_num));
+        } else {
+            println!("\nNo vaild option selected\n");
+            continue 'input_check;
+        }
+
+        break;
+    }
+}
+
+fn temp_converter(op: i8, deg: f64) -> f64 {
+    if op == 1 {
+        (deg - 32.0) / 1.8
+    } else {
+        (deg * 1.8) + 32.0
+    }
+}
